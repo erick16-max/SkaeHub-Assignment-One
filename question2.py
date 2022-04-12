1ff18aaf2c96c67689fae1cecbdd2b088717442a
erick16-max <erickgege16@gmail.com>
2022-04-12 11:16:08 +0300

    changed all programs to function based

--- question2.py
+++ question2.py
@@ -2,14 +2,20 @@
 
 str = input("Enter the sentence:")
 
-#converting string to list
-li = list(str.split(" "))
+def last_word_length(str):
+    #converting string to list
+    li = list(str.split(" "))
 
-#Get the last item of List
-last_word = li[-1]
+    #Get the last item of List
+    last_word = li[-1]
 
-#length of the word
-last_word_length = len(last_word)
-print(f" The length of the last word is {last_word_length}")
-   
+    #length of the word
+    last_word_length = len(last_word)
+    
+    return last_word_length
+
+
+last_word_length = last_word_length(str)
+print(f"The length of the last word is {last_word_length}")
+    
   
--- question4.py
+++ question4.py
@@ -1,17 +1,23 @@
 #Reading a csv file to a Dict
 
 import csv
+dict_data = []
+def csv_to_dict():
+    #path to csv
+    csvFile = "csv_data.csv"
 
-#path to csv
-csvFile = "csv_data.csv"
+    #Open the file
+    with open (csvFile) as csvFile:
+        #Dict object of the data
+        csvReader = csv.DictReader(csvFile)
 
-#Open the file
-with open (csvFile) as csvFile:
-    #Dict object of the data
-    csvReader = csv.DictReader(csvFile)
+        #iterate object to get dict rows
+        for rows in csvReader:
+            dict_data.append(rows)
+    #returning a list with the dict_data
+    return(dict_data)
+dict_data = csv_to_dict()
+print(dict_data)
 
-    #iterate object to get dict rows
-    for rows in csvReader:
-        print(rows)
         
 
