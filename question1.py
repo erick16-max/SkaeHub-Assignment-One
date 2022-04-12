285850289a255d4f3b612c7e3d0791808aa68ec4
erick16-max <erickgege16@gmail.com>
2022-04-12 11:05:11 +0300

    turning question one to a function program

--- question1.py
+++ question1.py
@@ -1,17 +1,21 @@
 #Get a Leap Year
 year =int(input ('Enter the year:'))
 
-#year is a century i.e divisible by 100
-if year % 100 == 0:
-#for a centuary to be a leap year it must be divible by 400
-    if year % 400 == 0:
-        print(True)
-    else:
-        print(False)
-# year is divible by 4
-elif year % 4 == 0:
-    print(True)
+def leap_year(year):
+    #year is a century i.e divisible by 100
+    if year % 100 == 0:
+    #for a centuary to be a leap year it must be divible by 400
+        if year % 400 == 0:
+            return True
+        else:
+            return False
+    # year is divible by 4
+    elif year % 4 == 0:
+        return True
 
-#if all the conditions are not met then the year is definately not a leap year
-else:
-    print(False)
+    #if all the conditions are not met then the year is definately not a leap year
+    else:
+        return False
+
+is_leap_year = leap_year(year)
+print(is_leap_year)
